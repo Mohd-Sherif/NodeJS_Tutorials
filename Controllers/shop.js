5316b7a479b46e1303cf9472b3fc892584bbbd15
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-07-26 21:37:33 +0300

    add getProduct

--- Controllers/shop.js
+++ Controllers/shop.js
@@ -10,6 +10,17 @@ exports.getProducts = (req, res, next) => {
     }).catch(err => console.log(err));
 }
 
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId).then(([product]) => {
+        res.render('shop/product-detail', {
+            product: product[0],
+            pageTitle: product.title,
+            path: '/products'
+        });
+    }).catch(err => console.log(err));
+}
+
 exports.getIndex = (req, res, next) => {
     Product.fetchAll().then(([rows, fieldData]) => {
         res.render('shop/index', {
