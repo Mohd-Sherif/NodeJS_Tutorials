5b542883fbbeb2cd51c99506c5bc4000f5fd4c62
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-07-26 23:58:50 +0300

    add more routers

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,6 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-
 const router = express.Router();
 
 const adminController = require('../Controllers/admin');
@@ -16,5 +14,11 @@ router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
