fe81c75799a03161b479b4e4313ac9dbabc57383
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-07-31 01:55:56 +0300

    use sequelize methods provided due to associtions

--- Controllers/admin.js
+++ Controllers/admin.js
@@ -16,7 +16,7 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    Product.create({
+    req.user.createProduct({
         title: title,
         price: price,
         imageUrl: imageUrl,
@@ -33,7 +33,9 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-    Product.findAll({where: {id: prodId}}).then(products => {
+    req.user.getProducts({
+        where: { id: prodId }
+    }).then(products => {
         if(!products){
             return res.redirect('/');
         }
@@ -65,7 +67,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.findAll().then(products => {
+    req.user.getProducts().then(products => {
         res.render('admin/products', {
             prods: products,
             pageTitle: 'Admin Products',
